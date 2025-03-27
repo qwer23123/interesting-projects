653cd8c9b64c3d6e04475dd6e8ca2b1f105f2e3d
Vadym Pidopryhora <withoutme5665@gmail.com>
2025-03-27 17:59:08 +0200

    new commit

--- nodeBasics/index2.js
+++ nodeBasics/index2.js
@@ -2,4 +2,4 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('Привіт, світ!'));
 console.log(chalk.red.bold('Це червоний жирний текст'));
-console.log(chalk.green.inverse('А це зелений на чорному14'));
+console.log(chalk.green.inverse('А це зелений на чорному1414'));
