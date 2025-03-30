ce55d6367a6f9894a0183294f40f44d8773bec01
Vadym Pidopryhora <withoutme5665@gmail.com>
2025-03-30 22:08:29 +0300

    task 4

---
+++ jsBasics/task4.js
@@ -0,0 +1,32 @@
+// Завдання 4.1
+let radius = 5;
+let circleArea = Math.PI * radius ** 2;
+circleArea = Number(circleArea.toFixed(2));
+
+console.log("=== Площа круга ===");
+console.log("Радіус:", radius);
+console.log("Площа:", circleArea);
+
+
+// Завдання 4.2
+let length = 10;
+let width = 4;
+let rectangleArea = length * width;
+rectangleArea = Number(rectangleArea.toFixed(2));
+
+console.log("\n=== Площа прямокутника ===");
+console.log("Довжина:", length);
+console.log("Ширина:", width);
+console.log("Площа:", rectangleArea);
+
+
+// Завдання 4.3
+let cylinderRadius = 3;
+let height = 7;
+let cylinderVolume = Math.PI * cylinderRadius ** 2 * height;
+cylinderVolume = Number(cylinderVolume.toFixed(2));
+
+console.log("\n=== Обʼєм циліндра ===");
+console.log("Радіус:", cylinderRadius);
+console.log("Висота:", height);
+console.log("Обʼєм:", cylinderVolume);
