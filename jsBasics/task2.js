1ffc86e3a6c2bd81764ac4d4f7a3641ac06bfa6d
Vadym Pidopryhora <withoutme5665@gmail.com>
2025-03-30 21:25:09 +0300

    task2

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let name1 = "Vadym";
+let name2 = "Alina";
+
+let myConcat = "Hello " + name1 + " i " + name2 + "!!!!!";
+console.log(myConcat);
+
+let myTemplate = `Hello, ${name1} і ${name2}!!!`;
+console.log(myTemplate);
